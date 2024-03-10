b26f2c240948b19262a0213067e28bff6c04c328
Swar Shah <90791181+swarshah09@users.noreply.github.com>
2024-03-11 04:20:39 +0530

    Update checkAuthToken.js

--- Middlewares/checkAuthToken.js
+++ Middlewares/checkAuthToken.js
@@ -12,27 +12,27 @@ function checkAuth(req, res, next) {
 
     jwt.verify(authToken, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if (err) {
-            // Auth token has expired, check the refresh token
-            jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET_KEY, (refreshErr, refreshDecoded) => {
-                if (refreshErr) {
-                    // Both tokens are invalid, send an error message and prompt for login
-                    return res.status(401).json({ message: 'Authentication failed: Both tokens are invalid', ok: false });
-                } else {
-                    // Generate new auth and refresh tokens
-                    const newAuthToken = jwt.sign({ userId: refreshDecoded.userId }, process.env.JWT_SECRET_KEY, { expiresIn: '10m' });
-                    const newRefreshToken = jwt.sign({ userId: refreshDecoded.userId }, process.env.JWT_REFRESH_SECRET_KEY, { expiresIn: '10d' });
-
-                    // Set the new tokens as cookies in the response
-                    res.cookie('authToken', newAuthToken, { httpOnly: true });
-                    res.cookie('refreshToken', newRefreshToken, { httpOnly: true }); //send back to cookies 
-
-                    // Continue processing the request with the new auth token
-                    req.userId = refreshDecoded.userId;
-                    req.ok = true;
-                    next();
-                }
-            });
+    // Error handling for expired or invalid authToken
+    jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET_KEY, (refreshErr, refreshDecoded) => {
+        if (refreshErr) {
+            // Both tokens are invalid
+            return res.status(401).json({ message: 'Authentication failed: Both tokens are invalid', ok: false });
         } else {
+            // Generate new auth and refresh tokens
+            const newAuthToken = jwt.sign({ userId: refreshDecoded.userId }, process.env.JWT_SECRET_KEY, { expiresIn: '10m' });
+            const newRefreshToken = jwt.sign({ userId: refreshDecoded.userId }, process.env.JWT_REFRESH_SECRET_KEY, { expiresIn: '10d' });
+
+            // Set the new tokens as cookies in the response
+            res.cookie('authToken', newAuthToken, { httpOnly: true });
+            res.cookie('refreshToken', newRefreshToken, { httpOnly: true });
+
+            // Continue processing the request with the new auth token
+            req.userId = refreshDecoded.userId;
+            req.ok = true;
+            next();
+        }
+    });
+} else {
             // Auth token is valid, continue with the request
             req.userId = decoded.userId;
             next();
